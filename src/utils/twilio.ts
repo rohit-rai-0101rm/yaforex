09bbba0f89f602386d2dc6f81578f32652d80674
rohit-rai-0101rm <rohitrai.dev@gmail.com>
2024-09-26 14:54:30 +0530

    deploying

--- src/utils/twilio.ts
+++ src/utils/twilio.ts
@@ -5,17 +5,17 @@ config(); // Load environment variables
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const verifyServiceSid = process.env.TWILIO_VERIFY_SERVICE_SID;
+const verifyServiceSid = process.env.TWILIO_VERIFY_SERVICE_SID!;
 
 const client = twilio(accountSid, authToken);
 
-export const sendVerificationCode = async (phoneNumber) => {
+export const sendVerificationCode = async (phoneNumber: string) => {
     return await client.verify.v2.services(verifyServiceSid)
         .verifications
         .create({ to: phoneNumber, channel: 'sms' });
 };
 
-export const verifyCode = async (phoneNumber, code) => {
+export const verifyCode = async (phoneNumber: string, code: string) => {
     return await client.verify.v2.services(verifyServiceSid)
         .verificationChecks
         .create({ to: phoneNumber, code });
